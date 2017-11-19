71fbd64967533cc2b9d577a54d8bceaf8859920e
Skye <CharlesBDunnIII@gmail.com>
2017-11-18 22:30:06 -0500

    Initial Commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ClassCalculater
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gradeRangeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.assignmentNameLabel = new System.Windows.Forms.Label();
+            this.gradeLabel = new System.Windows.Forms.Label();
+            this.percentOfTotalGradeLabel = new System.Windows.Forms.Label();
+            this.assignmentNumberInput = new System.Windows.Forms.MaskedTextBox();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip.Size = new System.Drawing.Size(1264, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gradeRangeToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // gradeRangeToolStripMenuItem
+            // 
+            this.gradeRangeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem1});
+            this.gradeRangeToolStripMenuItem.Name = "gradeRangeToolStripMenuItem";
+            this.gradeRangeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.gradeRangeToolStripMenuItem.Text = "Grade Range";
+            // 
+            // editToolStripMenuItem1
+            // 
+            this.editToolStripMenuItem1.Name = "editToolStripMenuItem1";
+            this.editToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.editToolStripMenuItem1.Text = "Edit";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of Assignments";
+            // 
+            // assignmentNameLabel
+            // 
+            this.assignmentNameLabel.AutoSize = true;
+            this.assignmentNameLabel.Location = new System.Drawing.Point(12, 129);
+            this.assignmentNameLabel.Name = "assignmentNameLabel";
+            this.assignmentNameLabel.Size = new System.Drawing.Size(139, 20);
+            this.assignmentNameLabel.TabIndex = 3;
+            this.assignmentNameLabel.Text = "Assignment Name";
+            // 
+            // gradeLabel
+            // 
+            this.gradeLabel.AutoSize = true;
+            this.gradeLabel.Location = new System.Drawing.Point(215, 129);
+            this.gradeLabel.Name = "gradeLabel";
+            this.gradeLabel.Size = new System.Drawing.Size(54, 20);
+            this.gradeLabel.TabIndex = 4;
+            this.gradeLabel.Text = "Grade";
+            // 
+            // percentOfTotalGradeLabel
+            // 
+            this.percentOfTotalGradeLabel.AutoSize = true;
+            this.percentOfTotalGradeLabel.Location = new System.Drawing.Point(314, 129);
+            this.percentOfTotalGradeLabel.Name = "percentOfTotalGradeLabel";
+            this.percentOfTotalGradeLabel.Size = new System.Drawing.Size(170, 20);
+            this.percentOfTotalGradeLabel.TabIndex = 5;
+            this.percentOfTotalGradeLabel.Text = "Percent of Total Grade";
+            // 
+            // assignmentNumberInput
+            // 
+            this.assignmentNumberInput.AsciiOnly = true;
+            this.assignmentNumberInput.BeepOnError = true;
+            this.assignmentNumberInput.Location = new System.Drawing.Point(219, 27);
+            this.assignmentNumberInput.Mask = "000";
+            this.assignmentNumberInput.Name = "assignmentNumberInput";
+            this.assignmentNumberInput.Size = new System.Drawing.Size(34, 26);
+            this.assignmentNumberInput.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.assignmentNumberInput);
+            this.Controls.Add(this.percentOfTotalGradeLabel);
+            this.Controls.Add(this.gradeLabel);
+            this.Controls.Add(this.assignmentNameLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "Class Calculator";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gradeRangeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label assignmentNameLabel;
+        private System.Windows.Forms.Label gradeLabel;
+        private System.Windows.Forms.Label percentOfTotalGradeLabel;
+        private System.Windows.Forms.MaskedTextBox assignmentNumberInput;
+    }
+}
+
