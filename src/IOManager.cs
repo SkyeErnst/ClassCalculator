012e079ac9130facaae25cdf1b2e38a4b53d93c7
Skye <Tarnthelos@gmail.com>
2018-03-04 23:24:43 -0500

    Added new file

---
+++ src/IOManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace ClassCalculater
+{
+    /// <summary>
+    /// Class for managing xml input and output
+    /// </summary>
+    public class IOManager
+    {
+        public IOManager()
+        {
+            string path = Assembly.GetExecutingAssembly().Location;
+
+
+        }
+    }
+}
